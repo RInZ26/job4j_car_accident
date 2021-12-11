3fcb5e39362127eb7cf5e77af43ce3259260f246
RinZ26 <ivs1996@mail.ru>
2021-12-11 13:40:23 +0500

    2. IndexControl. Таблица и вид. [#2092 #238511] fix CR

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.WebApplicationContext;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.service.AccidentService;
 
@@ -13,12 +12,11 @@ import java.util.List;
 @Controller
 public class IndexControl {
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    private AccidentService accidentService;
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Accident> accidents = webApplicationContext.getBean(AccidentService.class)
-                .getAllAccidents();
+        List<Accident> accidents = accidentService.getAllAccidents();
         model.addAttribute("accidents", accidents);
         return "index";
     }
