9cd0fe8a0f4642d333ff27d7c984c02d6e8b6833
RinZ26 <ivs1996@mail.ru>
2021-12-14 13:54:07 +0500

    Resurrect Mem

---
+++ src/main/java/ru/job4j/accident/repository/mem/AccidentMem.java
@@ -0,0 +1,38 @@
+package ru.job4j.accident.repository.mem;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.accident.model.Accident;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class AccidentMem {
+    /**
+     * Делаем вид, что у нас нет ORM и id мы проставляем сами
+     */
+    private final AtomicInteger identityCounter = new AtomicInteger(0);
+
+    private final Map<Integer, Accident> accidents = new HashMap<>();
+
+    public void save(Accident accident) {
+        int key = identityCounter.addAndGet(1);
+        accident.setId(key);
+        change(key, accident);
+    }
+
+    public void change(Integer id, Accident accident) {
+        accidents.put(id, accident);
+    }
+
+    public Accident findById(int id) {
+        return accidents.get(id);
+    }
+
+    public List<Accident> findAll() {
+        return new ArrayList<>(accidents.values());
+    }
+}
