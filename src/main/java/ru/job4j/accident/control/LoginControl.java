c1389de654414e4180d57a0fc46454464340c14b
RinZ26 <ivs1996@mail.ru>
2021-12-17 18:07:04 +0500

    2. Регистрация пользователя [#296069 #240695]

--- src/main/java/ru/job4j/accident/control/LoginControl.java
+++ src/main/java/ru/job4j/accident/control/LoginControl.java
@@ -6,8 +6,6 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginControl {
+
     @GetMapping("/login")
     public String loginPage(@RequestParam(value = "error", required = false) String error,
                             @RequestParam(value = "logout", required = false) String logout,
@@ -30,9 +29,10 @@ public class LoginControl {
         return "login";
     }
 
-    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    @GetMapping(value = "/logout")
     public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Authentication auth = SecurityContextHolder.getContext()
+                                                   .getAuthentication();
         if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
--- src/main/java/ru/job4j/accident/control/RegControl.java
+++ src/main/java/ru/job4j/accident/control/RegControl.java
@@ -1,37 +1,35 @@
 package ru.job4j.accident.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.accident.model.User;
-import ru.job4j.accident.repository.AuthorityRepository;
-import ru.job4j.accident.repository.UserRepository;
+import ru.job4j.accident.service.UserService;
 
 @Controller
 public class RegControl {
 
-    private final PasswordEncoder encoder;
-    private final UserRepository users;
-    private final AuthorityRepository authorities;
+    private final UserService userService;
 
     @Autowired
-    public RegControl(PasswordEncoder encoder, UserRepository users,
-                      AuthorityRepository authorities) {
-        this.encoder = encoder;
-        this.users = users;
-        this.authorities = authorities;
+    public RegControl(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
-        user.setEnabled(true);
-        user.setPassword(encoder.encode(user.getPassword()));
-        user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
-        return "redirect:/login";
+    public String regSave(@ModelAttribute User user, Model model) {
+        boolean isNewUser = userService.saveUser(user);
+
+        String errorMessage = "User with such name is already exist";
+
+        if (!isNewUser) {
+            model.addAttribute("errorMessage", errorMessage);
+        }
+
+        return isNewUser ?  "redirect:/login" : "reg";
     }
 
     @GetMapping("/reg")
